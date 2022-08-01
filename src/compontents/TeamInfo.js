08a7bb5237b53efb5b4ad96b4e29fa62ae9716ae
damir_kh <dadasay41@gmail.com>
2022-08-01 14:44:22 +0600

    init

--- src/compontents/TeamInfo.js
+++ src/compontents/TeamInfo.js
@@ -5,7 +5,19 @@ import {Col, Form} from "react-bootstrap"
 export const TeamInfo = ({number, scores, players, playersString, teamInfoSetter}) => {
 
     const setScores = (event) => {
-        scores = parseInt(event.target.value)
+        let scores = parseInt(event.target.value)
+
+        const minScores = 0
+        const maxScores = 7
+
+        if(scores < minScores){
+            scores = 0
+        }
+
+        if(scores > maxScores){
+            scores = 7
+        }
+
         event.target.value = scores
 
         teamInfoSetter({
@@ -15,12 +27,20 @@ export const TeamInfo = ({number, scores, players, playersString, teamInfoSetter
         })
     }
 
-    const setPlayers = (playersString) => {
+    const setPlayers = (event) => {
+        let newPlayersString = event.target.value
+        let newPlayers = newPlayersString.split('\n').map(item => item.trim()).filter(item => item.length)
+
+        if(newPlayers.length > 11){
+            event.target.value = playersString
+            return
+        }
+
         teamInfoSetter({
             number,
             scores,
-            players: playersString.split('\n').map(item => item.trim()).filter(item => item.length),
-            playersString
+            players: newPlayers,
+            playersString: newPlayersString
         })
     }
 
@@ -33,12 +53,15 @@ export const TeamInfo = ({number, scores, players, playersString, teamInfoSetter
                     <Form.Control id="playersList" as={"textarea"} style={{height: '200px'}}
                                   placeholder="Players List"
                                   value={playersString}
-                                  onChange={event => setPlayers(event.target.value)}
+                                  onChange={event => setPlayers(event)}
                     />
+                    {players.length == 11 && (
+                        <span className='text-danger'>You entered maximal number of players</span>
+                    )}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label className='ms-2' htmlFor="scores">Scores</Form.Label>
-                    <Form.Control id="Scores" type='number' min={1}
+                    <Form.Control id="Scores" type='number' min={0} max={7}
                                   placeholder="Scores"
                                   value={parseInt(scores)}
                                   onChange={event => setScores(event)}
